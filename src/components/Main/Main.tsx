5ebbe6d7045c357ca5a0d3e4a16642718e69c2f9
Evgeniy14141 <eugene-gr14@yandex.ru>
2024-10-02 00:39:34 +0400

    Авторизация и регистрация

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 export default function Main() {
   return (
-    <div>hello, LogIn, Reg, Profile</div>
+    <div className="text-3xl font-bold underline">hello, LogIn, Reg, Profile!!!!</div>
+    
   )
 }
---
+++ src/pages/NewPasswordPage.tsx
@@ -0,0 +1,13 @@
+import NewPassword from "../components/NewPassword/NewPassword";
+
+const NewPasswordPage = () => {
+  return (
+    <>
+      <div className="w-full h-full overflow-x-hidden bg-[#eaeef6]">
+        <NewPassword />
+      </div>
+    </>
+  );
+};
+
+export default NewPasswordPage;
